a30a1b679ff8f16723fc0f0bb0d0ba917700bcad
Lucian Maran <lucian.maran@outlook.com>
2017-07-06 22:31:52 +0300

    For link3, get data from API (node.js)

--- client/app/link3/link3.component.ts
+++ client/app/link3/link3.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
-  selector: 'app-link3',
-  templateUrl: './link3.component.html',
-  styleUrls: ['./link3.component.css']
+    selector: 'app-link3',
+    templateUrl: './link3.component.html',
+    styleUrls: ['./link3.component.css']
 })
 export class Link3Component implements OnInit {
+    data: Object;
+    apiData: Object;
+    loading: boolean;
 
-  constructor() { }
+    constructor(private http: Http) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.http.request('/api/user').subscribe((res: Response) => {
+            this.apiData = res.json();
+        });
+    }
+
+    makeRequest(): void {
+        this.loading = true; 
+        this.http.request('http://jsonplaceholder.typicode.com/posts/1').subscribe((res: Response) => {
+            this.data = res.json();
+            this.loading = false;
+        });
+    }
 
 }
+
